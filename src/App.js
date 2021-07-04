5b12615bc917384fe17b42e0e3301510d77bec8e
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-04 19:52:04 +0700

    menambahkan header, divide, dan label

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import {
+  Grid,
+  Flag,
+  Icon,
+  Input,
+  Header,
+  Image,
+  Container,
+  Divider,
+  Label,
+} from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Grid container verticalAlign="middle">
+          <Grid.Column width={1}>
+            <Flag name="id" size="large" />
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <Icon name="angle left" />
+          </Grid.Column>
+          <Grid.Column width={1}>
+            <Icon name="angle right" />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Input
+              fluid
+              icon="star"
+              size="large"
+              placeholder="Search..."
+              width="80%"
+            />
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <Header as="h3">
+              <Image
+                circular
+                src="https://react.semantic-ui.com/images/avatar/large/patrick.png"
+              />
+              Patrick
+            </Header>
+          </Grid.Column>
+        </Grid>
+        <Divider horizontal>SELAMAT DATANG</Divider>
+        <Grid container textAlign="right">
+          <Grid.Column>
+            <Label as="a" color="teal" tag>
+              Sport
+            </Label>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
